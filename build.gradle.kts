178c5519d3034f989b521cbb88f62f1ce9cc01ff
Alejomc26 <alechavezjaja26@gmail.com>
2024-03-31 18:19:21 -0500

    publish to hangar

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,53 @@
+import io.papermc.hangarpublishplugin.model.Platforms
+
 plugins {
     id("java")
+    id("io.papermc.hangar-publish-plugin") version "0.1.0"
 }
 
 group = "io.github.alejomc26"
-version = "1.0-SNAPSHOT"
+version = "1.0"
 
 repositories {
     mavenCentral()
     maven("https://repo.papermc.io/repository/maven-public/")
+}
+
+hangarPublish {
+    publications.register("plugin") {
+        version.set(project.version as String)
+        channel.set("Snapshot") // We're using the 'Snapshot' channel
+        // TODO: Edit the project name to match your Hangar project
+        id.set("hangar-project")
+        apiKey.set(System.getenv("HANGAR_API_TOKEN"))
+        platforms {
+            // TODO: Use the correct platform(s) for your plugin
+            register(Platforms.PAPER) {
+                // TODO: If you're using ShadowJar, replace the jar lines with the appropriate task:
+                //   jar.set(tasks.shadowJar.flatMap { it.archiveFile })
+                // Set the jar file to upload
+                jar.set(tasks.jar.flatMap { it.archiveFile })
+
+                // Set platform versions from gradle.properties file
+                val versions: List<String> = (property("paperVersion") as String)
+                        .split(",")
+                        .map { it.trim() }
+                platformVersions.set(versions)
+
+                // TODO: Configure your plugin dependencies, if any
+                dependencies {
+                    // Example for a dependency found on Hangar
+                    hangar("Maintenance") {
+                        required.set(false)
+                    }
+                    // Example for an external dependency
+                    url("Debuggery", "https://github.com/PaperMC/Debuggery") {
+                        required.set(true)
+                    }
+                }
+            }
+        }
+    }
 }
 
 dependencies {
